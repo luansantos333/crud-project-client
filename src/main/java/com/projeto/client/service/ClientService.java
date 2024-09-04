18695b4bb09dfd21b1b0a6e1fdca32f7f2b55b1b
Luan Gomes dos Santos <luangomespctu@gmail.com>
2024-09-04 10:50:11 -0300

    feat: add findById method

--- src/main/java/com/projeto/client/service/ClientService.java
+++ src/main/java/com/projeto/client/service/ClientService.java
@@ -1,9 +1,26 @@
 package com.projeto.client.service;
 
+import com.projeto.client.dto.ClientDTO;
+import com.projeto.client.repository.ClientRepo;
+import com.projeto.client.service.exceptions.ResourceNotFoundException;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class ClientService {
+
+    private final ClientRepo clientRepo;
+
+    public ClientService(ClientRepo clientRepo) {
+        this.clientRepo = clientRepo;
+    }
+
+    @Transactional (readOnly = true)
+    public ClientDTO findById (Long id) {
+       ClientDTO dto = new ClientDTO(clientRepo.findById(id).orElseThrow(() -> new ResourceNotFoundException("Couldn't found the client with the specified id")));
+        return dto;
+    }
+
 
 
 
