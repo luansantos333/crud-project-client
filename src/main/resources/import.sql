a900aecda73a6989b3c713d7f5a4276e09dcdd77
Luan Gomes dos Santos <luangomespctu@gmail.com>
2024-09-04 16:52:16 -0300

    feat: seed database

---
+++ src/main/resources/import.sql
@@ -0,0 +1,22 @@
+INSERT INTO tb_client (name, cpf, income, birth_date, children ) VALUES ('Otávio Augusto', '90403726085',10000.0, '2000-05-20', 2);
+
+INSERT INTO tb_client (name, cpf, income, birth_date, children ) VALUES ('Cornélio Sula', '90403726086',9000.0, '1998-07-20', 1);
+
+INSERT INTO tb_client (name, cpf, income, birth_date, children ) VALUES ('Quinto Sertório', '80403726086',9000.0, '1980-07-20', 3);
+
+INSERT INTO tb_client (name, cpf, income, birth_date, children ) VALUES ('Vespasiano', '92403726086',2000.0, '2003-08-21',4);
+
+INSERT INTO tb_client (name, cpf, income, birth_date, children ) VALUES ('Cipião Emiliano', '9240372602',3000.0, '2005-08-21',2);
+
+INSERT INTO tb_client (name, cpf, income, birth_date, children ) VALUES ('Germânico', '92403326086',4000.0, '2000-08-21',4);
+
+INSERT INTO tb_client (name, cpf, income, birth_date, children ) VALUES ('Fábio Máximo', '92403723086',2000.0, '1970-08-21',6);
+
+INSERT INTO tb_client (name, cpf, income, birth_date, children ) VALUES ('Anco Márcio', '92403726088',2000.0, '2003-08-21',4);
+
+INSERT INTO tb_client (name, cpf, income, birth_date, children ) VALUES ('Caio Júlio César', '72403726086',5000.0, '1999-08-21',4);
+
+INSERT INTO tb_client (name, cpf, income, birth_date, children ) VALUES ('Pompeu', '10403726086',5000.0, '2003-08-21',4);
+
+
+
