d2854120e871c3376280546e80e04c590bb72329
Luan Gomes dos Santos <luangomespctu@gmail.com>
2024-09-04 10:14:36 -0300

    Initial commit

---
+++ src/main/java/com/projeto/client/ClientCrudProjectApplication.java
@@ -0,0 +1,13 @@
+package com.projeto.client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientCrudProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClientCrudProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/projeto/client/ClientCrudProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projeto.client;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientCrudProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
