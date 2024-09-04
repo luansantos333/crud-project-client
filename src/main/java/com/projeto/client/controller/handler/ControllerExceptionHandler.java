c193206b21e2b48ee668dc465debe3e16b6eb0bd
Luan Gomes dos Santos <luangomespctu@gmail.com>
2024-09-04 17:36:38 -0300

    fix: add ExceptionHandler to MethodArgumentNotValidException

--- src/main/java/com/projeto/client/controller/handler/ControllerExceptionHandler.java
+++ src/main/java/com/projeto/client/controller/handler/ControllerExceptionHandler.java
@@ -1,10 +1,14 @@
 package com.projeto.client.controller.handler;
 
 import com.projeto.client.dto.CustomError;
+import com.projeto.client.dto.FieldMessage;
+import com.projeto.client.dto.ValidationError;
 import com.projeto.client.service.exceptions.ResourceNotFoundException;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -12,13 +16,26 @@ import java.time.Instant;
 
 @ControllerAdvice
 public class ControllerExceptionHandler {
-
-
     @ExceptionHandler(ResourceNotFoundException.class)
     public ResponseEntity<CustomError> resourceNotFound (ResourceNotFoundException e, HttpServletRequest request) {
-        HttpStatus status = HttpStatus.BAD_REQUEST;
+        HttpStatus status = HttpStatus.NOT_FOUND;
         CustomError err = new CustomError(Instant.now(), status.value(), e.getMessage(), request.getRequestURI());
         return ResponseEntity.status(status).body(err);
     }
+
+    @ExceptionHandler (MethodArgumentNotValidException.class)
+    public ResponseEntity<CustomError> methodArgumentNotValid (MethodArgumentNotValidException e, HttpServletRequest request) {
+
+        HttpStatus status = HttpStatus.UNPROCESSABLE_ENTITY;
+        ValidationError err = new ValidationError(Instant.now(), status.value(), "Erro de validação", request.getRequestURI());
+
+        for (FieldError error : e.getBindingResult().getFieldErrors()) {
+            err.getErrorMessages().add(new FieldMessage(error.getField(), error.getDefaultMessage()));
+        }
+
+        return ResponseEntity.status(status).body(err);
+
+    }
+
 
 }
