a05664545da160ce4fba2533e5bd1086f386849a
Luan Gomes dos Santos <luangomespctu@gmail.com>
2024-09-04 10:50:27 -0300

    feat: add findById method returning DTO object

--- src/main/java/com/projeto/client/controller/ClientController.java
+++ src/main/java/com/projeto/client/controller/ClientController.java
@@ -1,4 +1,33 @@
 package com.projeto.client.controller;
 
+import com.projeto.client.dto.ClientDTO;
+import com.projeto.client.service.ClientService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+
+@RestController
+@RequestMapping("/client")
 public class ClientController {
+
+    private final ClientService clientService;
+
+    public ClientController(ClientService clientService) {
+        this.clientService = clientService;
+    }
+
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<ClientDTO> findById(@PathVariable(value = "id") Long id) {
+        ClientDTO dto = clientService.findById(id);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(dto.getId()).toUri();
+        return ResponseEntity.created(uri).body(dto);
+    }
+
+
 }
