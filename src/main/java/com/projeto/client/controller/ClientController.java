90a3dc2b68eceb7a8657ee57ba1f0ab41520ac95
Luan Gomes dos Santos <luangomespctu@gmail.com>
2024-09-04 16:55:10 -0300

    feat: add CRUD operations for Controller class

--- src/main/java/com/projeto/client/controller/ClientController.java
+++ src/main/java/com/projeto/client/controller/ClientController.java
@@ -2,11 +2,13 @@ package com.projeto.client.controller;
 
 import com.projeto.client.dto.ClientDTO;
 import com.projeto.client.service.ClientService;
+import jakarta.servlet.http.HttpServletRequest;
+import org.apache.coyote.Response;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.net.URI;
@@ -21,7 +23,6 @@ public class ClientController {
         this.clientService = clientService;
     }
 
-
     @GetMapping(value = "/{id}")
     public ResponseEntity<ClientDTO> findById(@PathVariable(value = "id") Long id) {
         ClientDTO dto = clientService.findById(id);
@@ -29,5 +30,29 @@ public class ClientController {
         return ResponseEntity.created(uri).body(dto);
     }
 
+    @GetMapping
+    public ResponseEntity<Page<ClientDTO>> findAll (Pageable p) {
+        return ResponseEntity.ok(clientService.findAll(p));
+    }
+
+    @PostMapping
+    public ResponseEntity<ClientDTO> addClient (@RequestBody ClientDTO c) {
+
+        ClientDTO dto = clientService.addNewClient(c);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(c.getId()).toUri();
+        return ResponseEntity.created(uri).body(dto);
+    }
+
+    @PutMapping ("/{id}")
+    public ResponseEntity<ClientDTO> updateClientById (@PathVariable (value = "id") Long id, @RequestBody ClientDTO clientDTO) {
+        ClientDTO updateClient = clientService.updateClient(id, clientDTO);
+        return ResponseEntity.status(HttpStatus.CREATED).body(updateClient);
+
+    }
+
+    @DeleteMapping ("/{id}")
+    public void deleteClientById (@PathVariable (value = "id") Long id) {
+        clientService.deleteClient(id);
+    }
 
 }
