804b5ae9d0cd0787d263b98ebc6d6843629f7377
Luan Gomes dos Santos <luangomespctu@gmail.com>
2024-09-04 16:52:28 -0300

    feat: add CustomError body class

---
+++ src/main/java/com/projeto/client/dto/CustomError.java
@@ -0,0 +1,19 @@
+package com.projeto.client.dto;
+
+import java.time.Instant;
+
+public class CustomError {
+
+    private Instant timestamp;
+    private Integer status;
+    private String error;
+    private String path;
+
+
+    public CustomError(Instant timestamp, Integer status, String error, String path) {
+        this.timestamp = timestamp;
+        this.status = status;
+        this.error = error;
+        this.path = path;
+    }
+}
